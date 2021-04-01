cc8cb9e6a03da66efd75821129a105fa22c21f48
Zena-B <65442614+Zena-B@users.noreply.github.com>
2021-04-01 21:11:35 +0530

    Update admin_result.php

--- website/sanctum/admin/admin_result.php
+++ website/sanctum/admin/admin_result.php
@@ -1,5 +1,5 @@
 <?php
-    $title="Result Management";
+    $title="Result";
     require('header.php');
 
     $sql_view='select * from result;';
@@ -17,92 +17,93 @@
             <div id="content">
 
                 <div class="container-fluid" >
-                <style>
-                    .table-hover tbody tr:hover td,
-                    .table-hover tbody tr:hover th {
-                        font-weight :700 ; 
-                        background-color: rgba(0, 0, 0, 0.32);
-                        color:white;
-                    }   
-                    .pagination > li > a
-                    {
-                        background-color: white;
-                        color:#261903;
-                    }
-                    .pagination > li > a:focus,
-                    .pagination > li > a:hover,
-                    .pagination > li > span:focus,
-                    .pagination > li > span:hover
-                    {
-                        color: #FFBB02;
-                        background-color: #eee;
-                        border-color:NONE;
-                    }
-
-                    .pagination > .active > a
-                    {
-                        color: white;
-                        background-color: #FFBB02 !Important;
-                        border: solid 1px #FFBB02 !Important;
-                    }
-
-                    .pagination > .active > a:hover
-                    {
-                        background-color: #FFBB02 !Important;
-                        border: solid 1px #FFBB02;
-                    }
+               
+                <div class="row mb-2">
+                    <button class="btn btn-warning dropdown-toggle" style="background-color:#ffbb02;border:none;text-align:left;width:100%" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <?php echo (isset($_GET['tournament_name']))? $_GET['tournament_name'] :"Select Tournament"; ?>
+                    </button>
+                </div>
+                <div class="card row mb-4" style="color:#261903;border:none;background-color:#6E9673">
                     
-                </style>
-                    
-                <div class="card  mb-4" style="color:#261903;border:none;background-color:#6E9673">
+                    <div>
+                        <br>
+                        <h2 style="color:white;text-align:center" >April Season</h2>
+                        <hr>
+                        <div class="row text-white">
+                            <div class="col-sm-6" style="padding-left:10rem;">
+                                <label style="font-weight:bold">Tournament Id:</label>
+                                <label>3220</label><br>
+                                <label style="font-weight:bold">Start Date:</label>
+                                <label>12.05.2021</label>
+                            </div>
+                            <div class="col-sm-6 " style="padding-right:10rem;text-align:right">
+                                <label style="font-weight:bold">Tournament Name:</label>
+                                <label>April Season</label><br>
+                                <label style="font-weight:bold">End Date:</label>
+                                <label>11.06.2021</label>
+                            </div>
+                        </div>
+                        <div class="row mt-5">
+                            <div class="col-xl-1 mb-4" ></div>
+                            <!-- 1st -->
+                            <div class="col-xl-3 col-md-6 mb-4" >
+                                <div class="card shadow h-100 py-2 bg-info">
+                                    <div class="card-body" style="border:none">
+                                        <div class="no-gutters justify-content-center text-center">
+                                            <div class="col-auto text-white" >
+                                                <h2 class="font-weight-bold">1st</h2>
+                                            </div><br>
+                                            <div class="col-auto">
+                                                <div class="mb-3"><img src='".$fetch_photo."' height='50' width='50' style='border-radius:50%;' /></div>
+                                                <div class="h3 mb-1 text-white mt-1">Player Name</div>
+                                                <div class="mb-1 mt-1" style="color:grey">Player id</div>
+                                                <div class="text-s font-weight-bold mt-3 mb-1 text-white">320010</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
 
-                    <div class="card-body ">
-                        <div class="table-responsive">
-                            <button class="btn btn-warning" style="float:right;background-color:#ffbb02;border:none;" >Release</button>
-                            <table class="table table-hover" id="dataTable" style="color:#261903;" width="100%" cellspacing="0">
-                                <thead>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Tournament_Id</th>
-                                        <th>Client_Id</th>
-                                        <th>Status</th>
-                                        <th>Position</th>
-                                    </tr>
-                                </thead>
-                                <tfoot>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Tournament_Id</th>
-                                        <th>Client_Id</th>
-                                        <th>Status</th>
-                                        <th>Position</th>
-                                    </tr>
-                                </tfoot>
-                                <tbody>
-                                    <?php
-                                    if($resultcheck>0){
-                                        while($row=mysqli_fetch_assoc($result)){
-                                            $fetch_result_id=$row["RESULT_ID"];
-                                            $fetch_tournament_id=$row["TOURNAMENT_ID"];
-                                            $fetch_client_id=$row["CLIENT_ID"];
-                                            $fetch_reward_status=$row["REWARD_STATUS"];
-                                            $fetch_position=$row["POSITION"];
+                            <!-- 2nd -->
+                            <div class="col-xl-3 col-md-6 mb-4 ml-5">
+                                <div class="card shadow h-100 py-2 bg-info">
+                                    <div class="card-body" style="border:none">
+                                        <div class="no-gutters justify-content-center text-center">
+                                            <div class="col-auto text-white" >
+                                                <h2 class="font-weight-bold">2nd</h2>
+                                            </div><br>
+                                            <div class="col-auto">
+                                                <div class="mb-3"><img src='".$fetch_photo."' height='50' width='50' style='border-radius:50%;' /></div>
+                                                <div class="h3 mb-1 text-white mt-1">Player Name</div>
+                                                <div class="mb-1 mt-1" style="color:grey">Player id</div>
+                                                <div class="text-s font-weight-bold mt-3 mb-1 text-white">320010</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
 
-                                            
-                                            echo"<tr>
-                                                <td>".$fetch_result_id."</td>
-                                                <td>".$fetch_tournament_id."</td>
-                                                <td>".$fetch_client_id."</td>
-                                                <td>".$fetch_reward_status."</td>
-                                                <td>".$fetch_position."</td>
-                                                </tr>";
-                                        }
-                                    }
-                                    ?>
-                                </tbody>
-                            </table>
+                            <!-- 3rd -->
+                            <div class="col-xl-3 col-md-6 mb-4 ml-5" >
+                                <div class="card shadow h-100 py-2 bg-info">
+                                    <div class="card-body" style="border:none">
+                                        <div class="no-gutters justify-content-center text-center">
+                                            <div class="col-auto text-white" >
+                                                <h2 class="font-weight-bold">3rd</h2>
+                                            </div><br>
+                                            <div class="col-auto">
+                                                <div class="mb-3"><img src='".$fetch_photo."' height='50' width='50' style='border-radius:50%;' /></div>
+                                                <div class="h3 mb-1 text-white mt-1">Player Name</div>
+                                                <div class="mb-1 mt-1" style="color:grey">Player id</div>
+                                                <div class="text-s font-weight-bold mt-3 mb-1 text-white">320010</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
+
                 </div>
             </div>
             </div>
