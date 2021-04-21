e4f9b40e0bfa50dd1689ee29414ba393fed03897
nilaykthakkar <79744812+nilaykthakkar@users.noreply.github.com>
2021-04-21 15:20:08 +0530

    Update client_tournament.php

--- website/sanctum/client/client_tournament.php
+++ website/sanctum/client/client_tournament.php
@@ -1,22 +1,85 @@
 <?php
     $title="Scoreboard";
     require_once('client_header.php');
-
-    $tournament_id=0;
-    if(isset($_GET['tournament_id']))
+    function dateCheck($tournament_end_date)
     {
-        $tournament_id=$_GET['tournament_id'];
-    }
-    $query="select c.CLIENT_PROFILE_PHOTO,s.CLIENT_ID,c.CLIENT_NAME,s.TOURNAMENT_ID,sum(s.SCORE_TOTAL)as 'SCORE_TOTAL',c.CLIENT_TOTAL_SCORE from scoreboard as s,
-                        client as c,tournament as t
-                        where 
-                        s.CLIENT_ID = c.CLIENT_ID 
-                        AND s.TOURNAMENT_ID = t.TOURNAMENT_ID 
-                        AND s.TOURNAMENT_ID = ".(int)$tournament_id." group by s.CLIENT_ID order by SCORE_TOTAL DESC;";
+        $date=substr($tournament_end_date,8,2);
+	    $month=substr($tournament_end_date,5,2);
+	    $year=substr($tournament_end_date,0,4);
 
-    $result=mysqli_query($conn,$query);
-    $resultcheck=mysqli_num_rows($result);
-    
+        //echo date("Y-m-d")." --- ".$date."-".$month."-".$year;
+
+        $today=getdate();
+        $today_date=$today['mday'];
+		$today_month=date('m',strtotime($today['month']));
+		$today_year=$today['year'];
+		
+		if($year>$today_year)
+		{
+			//$error_date="It is a Future Date.";
+            return 1;
+		}
+		else if($year<$today_year)
+		{
+			//$error_date="It is a Past Date.";
+            return -1;
+		}
+		else
+		{
+			if($month>$today_month)
+			{
+				//$error_date="It is a Future Date.";
+                return 1;
+			}
+			else if($month<$today_month)
+			{
+				//$error_date="It is a Past Date.";
+                return -1;
+			}
+			else
+			{
+				if($date>$today_date)
+				{
+					//$error_date="It is a Future Date.";
+                    return 1;
+				}
+				else if($date<$today_date)
+				{
+					//$error_date="It is a Past Date.";
+                    return -1;
+				}
+				else
+				{
+					//$error_date="It is Present date.";
+                    return 0;
+				}
+			}
+		}
+        //echo "<br><h1>".$error_date."</h1>";
+
+    }
+
+    function get_tournament_id()
+    {
+        require('../backend assets/connection.php');
+
+        $query="select * from tournament order by TOURNAMENT_ID DESC;";
+
+        $result=mysqli_query($conn,$query);
+
+        while($row=mysqli_fetch_array($result))
+        {
+            $start_val=dateCheck($row['TOURNAMENT_START']);
+            $end_val=dateCheck($row['TOURNAMENT_END']);
+
+            if(($start_val==0) || ($end_val==1 && $start_val==-1) || $end_val==0)
+            {
+                return (int)$row['TOURNAMENT_ID'];
+            }
+        }
+        return 0;
+    }
+	$current_tournament_id=get_tournament_id();
    
 ?>
 
@@ -75,15 +138,11 @@
                             <div class="table-responsive">
                                 <div class="dropdown">
                                     <button class="btn btn-warning dropdown-toggle" style="float:right;background-color:#ffbb02;border:none;text-align:left" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <?php echo (isset($_GET['tournament_name']))? $_GET['tournament_name'] :"Select Tournament"; ?>
+                                        <?php echo (isset($_GET['a']))? $_GET['a'] :"Select Tournament"; ?>
                                     </button>
                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                    <?php
-                                        $query_tournament="select * from tournament;";
-                                        $data=mysqli_query($conn,$query_tournament);
-                                        while($row=mysqli_fetch_array($data))
-                                            echo '<a class="dropdown-item" href=client_tournament.php?tournament_id='.$row["TOURNAMENT_ID"].'&tournament_name='.urlencode($row["TOURNAMENT_NAME"]).'>'.$row["TOURNAMENT_NAME"].'</a>';
-                                    ?>
+                                    <a class="dropdown-item" href=client_tournament.php?a=current>CURRENT</a>
+                                    <a class="dropdown-item" href=client_tournament.php?a=previous>PREVIOUS</a>
                                     </div>
                                 </div>
                                 <table class="table table-hover" id="dataTable" style="color:#261903;" width="100%" cellspacing="0">
@@ -91,11 +150,11 @@
                                     <thead>
                                         <tr>                                           
                                             <th>RANK</th>
-                                            <th>CLIENT ID</th>
+                                            <th>TOURNAMENT ID</th>
+                                            <th>TOURNAMENT NAME</th>
                                             <th>PROFILE</th>
                                             <th>NAME</th>
                                             <th>SCORE</th>
-                                            <th>TOTAL SCORE</th>
                                         </tr>
                                     </thead>
                                     <!-- <tfoot>
@@ -109,22 +168,48 @@
                                     </tfoot> -->
                                     <tbody>
                                         <?php
-                                        if($resultcheck>0)
+                                        if(isset($_GET['a']))
                                         {
-                                            $rank=0;
-                                            while($row=mysqli_fetch_assoc($result))
-                                            {
-                                                ++$rank;
-												if($row["CLIENT_ID"]==$_SESSION['client_id'])
-                                                echo'<tr>
+											$query_tournament="select * from tournament;";
+											$data=mysqli_query($conn,$query_tournament);
+											while($row1=mysqli_fetch_array($data))
+											{
+												$tournament_id=$row1[0];
+												$tournament_name=$row1[1];
+												$query1="select sum(SCORE_TOTAL) from scoreboard where CLIENT_ID={$_SESSION['client_id']} and TOURNAMENT_ID=$tournament_id";
+												$result1=mysqli_query($conn,$query1);
+												$row1=mysqli_fetch_array($result1);
+												
+												
+												$query2="select CLIENT_ID,sum(SCORE_TOTAL) from scoreboard where TOURNAMENT_ID=$tournament_id group by CLIENT_ID";
+												//echo $query2;
+												$result2=mysqli_query($conn,$query2);
+												$rank=1;
+												while($row=mysqli_fetch_array($result2))
+												{
+													//echo $row[0];
+													if($row[1]>$row1[0])
+														++$rank;
+												}
+												$query2="select * from scoreboard where TOURNAMENT_ID=$tournament_id group by CLIENT_ID  ";
+												//echo $query2;
+												$result2=mysqli_query($conn,$query2);
+												while($row=mysqli_fetch_array($result2))
+												{
+													if(($_GET['a']=="current" && $tournament_id==$current_tournament_id) || ($_GET['a']=="previous" && $tournament_id!=$current_tournament_id))
+													if($row["CLIENT_ID"]==$_SESSION['client_id'] )
+													{
+													echo'<tr>
                                                     <td>'.$rank.'</td>
-                                                    <td>'.$row["CLIENT_ID"].'</td>
-                                                    <td><img style='."height:2.5rem;width:2.5rem;border-radius:50%;".' src="'.$row["CLIENT_PROFILE_PHOTO"].'" ></td>
-                                                    <td>'.$row["CLIENT_NAME"].'</td>
-                                                    <td>'.$row["SCORE_TOTAL"].'</td>
-                                                    <td>'.$row["CLIENT_TOTAL_SCORE"].'</td>
+                                                    <td>'.$tournament_id.'</td>
+                                                    <td>'.$tournament_name.'</td>
+                                                    <td><img style='."height:2.5rem;width:2.5rem;border-radius:50%;".' src="'.$_SESSION["client_profile_photo"].'" ></td>
+                                                    <td>'.$_SESSION["client_name"].'</td>
+                                                    <td>'.$row1[0].'</td>
                                                     </tr>';
-                                            }
+													}
+												}
+											}
                                         }
                                         ?>
                                     </tbody>
