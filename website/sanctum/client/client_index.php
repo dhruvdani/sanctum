e0e156ae707061438e1bc6f82a2c3796e3486af4
nilaykthakkar <79744812+nilaykthakkar@users.noreply.github.com>
2021-04-21 15:45:12 +0530

    Update client_index.php

--- website/sanctum/client/client_index.php
+++ website/sanctum/client/client_index.php
@@ -2,8 +2,105 @@
     $title="Welcome";
 	//session_start();
     require('client_header.php'); 
-	
     //$row['ADM_PROFILE_PHOTO']="../images/t3.jpg";
+	$query="select CLIENT_SANCTUM_TOKEN from client where CLIENT_ID={$_SESSION['client_id']} ";
+	$result=mysqli_query($conn,$query);
+	$row=mysqli_fetch_array($result);
+	
+	$current_tournament=get_tournament_id();
+	echo $current_tournament;
+	if($current_tournament!=0)
+	{
+		$query1="select sum(SCORE_TOTAL) from scoreboard where CLIENT_ID={$_SESSION['client_id']} and TOURNAMENT_ID=$current_tournament";
+		$result1=mysqli_query($conn,$query1);
+		$row1=mysqli_fetch_array($result1);
+		$value=$row1[0];
+	}
+	else
+		$value="---";
+	
+	$query2="select sum(SCORE_TOTAL) from scoreboard where CLIENT_ID={$_SESSION['client_id']} ";
+	$result2=mysqli_query($conn,$query2);
+	$row2=mysqli_fetch_array($result2);
+	
+	function dateCheck($tournament_end_date)
+    {
+        $date=substr($tournament_end_date,8,2);
+	    $month=substr($tournament_end_date,5,2);
+	    $year=substr($tournament_end_date,0,4);
+
+        //echo date("Y-m-d")." --- ".$date."-".$month."-".$year;
+
+        $today=getdate();
+        $today_date=$today['mday'];
+		$today_month=date('m',strtotime($today['month']));
+		$today_year=$today['year'];
+		
+		if($year>$today_year)
+		{
+			//$error_date="It is a Future Date.";
+            return 1;
+		}
+		else if($year<$today_year)
+		{
+			//$error_date="It is a Past Date.";
+            return -1;
+		}
+		else
+		{
+			if($month>$today_month)
+			{
+				//$error_date="It is a Future Date.";
+                return 1;
+			}
+			else if($month<$today_month)
+			{
+				//$error_date="It is a Past Date.";
+                return -1;
+			}
+			else
+			{
+				if($date>$today_date)
+				{
+					//$error_date="It is a Future Date.";
+                    return 1;
+				}
+				else if($date<$today_date)
+				{
+					//$error_date="It is a Past Date.";
+                    return -1;
+				}
+				else
+				{
+					//$error_date="It is Present date.";
+                    return 0;
+				}
+			}
+		}
+        //echo "<br><h1>".$error_date."</h1>";
+
+    }
+
+    function get_tournament_id()
+    {
+        require('../backend assets/connection.php');
+
+        $query="select * from tournament order by TOURNAMENT_ID DESC;";
+
+        $result=mysqli_query($conn,$query);
+
+        while($row=mysqli_fetch_array($result))
+        {
+            $start_val=dateCheck($row['TOURNAMENT_START']);
+            $end_val=dateCheck($row['TOURNAMENT_END']);
+
+            if(($start_val==0) || ($end_val==1 && $start_val==-1) || $end_val==0)
+            {
+                return (int)$row['TOURNAMENT_ID'];
+            }
+        }
+        return 0;
+    }
 ?>
 
     <style>
@@ -28,9 +125,7 @@
         }
         .out .imgg{
             position:relative;
-            bottom:-50%;
-			height:120%;
-			width:50%;
+            bottom:0;
         }
         h2{display:block;}
     </style>
@@ -40,7 +135,7 @@
 
         <div class="row f" ">
             <div class="out g" style="text-align:center">
-                <img src=<?php echo $_SESSION['client_profile_photo'];?> class="imgg">
+                <img src="../images/ts1.jpg"  class="imgg">
             </div>
            
         </div>
@@ -65,7 +160,7 @@
                             </div><br>
                             <div class="col-auto">
                             <div class="mb-3"><i class="fa fa-bolt text-white" style="font-size:60px;color:orange`" aria-hidden="true"></i></div>
-                            <br><div class="h1 mb-e mt-1">352</div>
+                            <br><div class="h1 mb-e mt-1"><?php echo $row[0]?></div>
                             </div>
                         </div>
                     </div>
@@ -80,7 +175,7 @@
                             </div><br>
                             <div class="col-auto">
                             <div class="mb-3"><i class="fa fa-trophy text-white" style="font-size:60px;color:orange`" aria-hidden="true"></i></div>
-                            <br><div class="h1 mb-1 mt-1">14590</div>
+                            <br><div class="h1 mb-1 mt-1"><?php echo $value?></div>
                             </div>
                         </div>
                     </div>
@@ -95,7 +190,7 @@
                             </div><br>
                             <div class="col-auto">
                             <div class="mb-3"><i class="fa fa-star-half text-white" style="font-size:60px;color:orange`" aria-hidden="true"></i></div>
-                            <br><div class="h1 mb-1 mt-1">90000</div>
+                            <br><div class="h1 mb-1 mt-1"><?php echo $row2[0]?></div>
                             </div>
                         </div>
                     </div>
