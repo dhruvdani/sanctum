4f96890b29094607bdccbd49a256fee147e0002d
dhruvdani <62289556+dhruvdani@users.noreply.github.com>
2021-04-22 20:57:35 +0530

    z

--- website/sanctum/backend assets/connection.php
+++ website/sanctum/backend assets/connection.php
@@ -1,8 +1,8 @@
 <?php
-    $host='localhost';
-    $database='sanctum_db';
-    $password='';
-    $username='root';
+    $host='sql307.epizy.com';
+    $database='epiz_28442673_sanctum_db';
+    $password='9wxklTtHlGulm';
+    $username='epiz_28442673';
 
 
     $conn=mysqli_connect($host,$username,$password,$database,3306);
