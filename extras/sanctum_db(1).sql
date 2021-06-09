af6b9d240fa38ff59b52d802acb5e4b297ad60dc
nilaykthakkar <79744812+nilaykthakkar@users.noreply.github.com>
2021-06-10 00:59:32 +0530

    database

--- extras/sanctum_db(1).sql
+++ extras/sanctum_db(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 18, 2021 at 06:16 PM
+-- Generation Time: Jun 09, 2021 at 09:27 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -45,8 +45,9 @@ CREATE TABLE `administrator` (
 --
 
 INSERT INTO `administrator` (`ADMIN_ID`, `ADMIN_NAME`, `ADMIN_EMAIL`, `ADMIN_CONTACT_1`, `ADMIN_CONTACT_2`, `ADMIN_MESSAGE`, `ADMIN_PASSWORD`, `ADMIN_PROFILE_PHOTO`, `ADMIN_USERNAME`, `ADMIN_RECOVERY_PIN`) VALUES
-(1, 'dhruv dani', 'dhruvbdani@gmail.com', '7621999436', '9320099436', 'dhruv is my name', 'dhruv123', '/client/client_images/d.jpg', 'dhruv.admin', '12abcd'),
-(2, 'Zaneta Bhagwagar', 'zanetabhagwagar@gmail.com', '7433005558', '', 'Zaneta', 'zaneta123', '/images/t3.jpg', 'zaneta.admin', '123abc');
+(1, 'Dhruv Dani', 'dhruvbdani@gmail.com', '9845213784', '9956784129', 'carpediam', 'dhruv', '/admin/admin_profile/sanctum_default.png', 'Dhruv', '45'),
+(2, 'Zena', 'zena@gmail.com', '9564789542', '9634257958', 'conspious', 'zena', '/admin/admin_profile/sanctum_default.png', 'Zena', '50'),
+(3, 'Nilay Thakkar', 'nilay2620@gmail.com', '9727945621', '9537505500', 'Hey There', 'nilay', '/admin/admin_profile/sanctum_default.png', 'Nilay', '20');
 
 -- --------------------------------------------------------
 
@@ -71,17 +72,16 @@ CREATE TABLE `client` (
 --
 
 INSERT INTO `client` (`CLIENT_ID`, `CLIENT_NAME`, `CLIENT_EMAIL`, `CLIENT_CONTACT`, `CLIENT_PASSWORD`, `CLIENT_STATUS`, `CLIENT_PROFILE_PHOTO`, `CLIENT_SANCTUM_TOKEN`, `CLIENT_TOTAL_SCORE`) VALUES
-(1, 'DHRUV DANI', 'DHRUVBDANI@GMAIL.COM', '7621999436', 'dhruv@2000', 1, '/images/t2.jpg', 0, 200),
-(2, 'ZANETA ', 'ZANETA@GMAIL.COM', '98765432S1', 'zena', 0, '/images/t3.jpg', 0, 0),
-(3, 'NILAYnil123', 'nilay2620@gmail.com', '9727945621', 'NILAY', 1, '/images/t4.jpg', 0, 0),
-(5, 'alpha', 'ALPHA@GMAIL.COM', '', 'alpha', 1, '\\backend assets\\images\\favicon.ico', 0, 0),
-(6, 'Nilay', 'NILAY2620@GMAIL.COM', '', 'nilaythakkar', 1, '/client/client_images/t4.jpg', 0, 0),
-(7, 'nilay2620@gmail.com', 'NILAYTHAKKAR2620@GMAIL.COM', '', 'nilaythakkar', 1, '/backend assets/images/favicon.ico', 0, 0),
-(9, 'feyash', 'FMODI@GMAIL.COM', '', 'feyash1', 1, '/client/client_images/sanctum-default.png', 0, 0),
-(10, 'Jay', 'NILAYNILAY@GMAIL.COM', '', 'd', 1, '/client/client_images/sanctum-default.png', 0, 0),
-(11, 'thakkar', 'thakkar@gmail.com', '', 'thakkar', 1, '/client/client_images/sanctum-default.png', 0, 0),
-(13, 'thakkar', 'thakkar12@gmail.com', '', 'thakkar', 1, '/client/client_images/sanctum-default.png', 0, 0),
-(14, 'thakkar1', 'thakkar123@gmail.com', '', 'thakkar', 1, '/client/client_images/Silver.jpg', 0, 0);
+(1, 'Jay n gujarati', 'jay25@gmail.com', '9845732168', 'jaygujarati12', 1, '/client/client_images/sanctum_default.png', 200, 20000),
+(2, 'vir patel', 'patel456@gmail.com', '9564237814', 'mindpatel98', 1, '/client/client_images/sanctum_default.png', 1000, 8000),
+(3, 'gaurav patel', 'gauravp56@gmail.com', '9959837814', 'gauravio@gmail.com', 1, '/client/client_images/sanctum_default.png', 3000, 15000),
+(4, 'Akshar surti', 'suratiak@gmail.com', '9834256178', 'akshar1254', 1, '/client/client_images/sanctum-default.png', 500, 14000),
+(5, 'jill shukla', 'jill@gmail.com', '945123784', 'jillshukla', 1, '/client/client_images/sanctum_default.png', 3000, 8500),
+(6, 'disha salian', 'dishas@gmail.com', '9564231456', 'khghf19842', 1, '/client/client_images/sanctum_default.png', 500, 900),
+(7, 'manushree patil', 'patil@gmail.com', '9865437841', 'mp8794patil', 1, '/client/client_images/sanctum_default.png', 5000, 17500),
+(8, 'shruti amrute', 'shramru@gmail.com', '9865454641', 'shruti564amrute', 1, '/client/client_images/sanctum_default.png', 2500, 10000),
+(9, 'mansi chiplunkar', 'mansi@gmail.com', '9995474641', 'mansichip', 1, '/client/client_images/sanctum_default.png', 1000, 19000),
+(10, 'komal rathi', 'rathi@gmail.com', '9564111234', 'komalgovind', 1, '/client/client_images/sanctum_default.png', 3500, 21000);
 
 -- --------------------------------------------------------
 
@@ -97,16 +97,6 @@ CREATE TABLE `feedback` (
   `FEEDBACK_MESSAGE` text NOT NULL,
   `FEEDBACK_TIME` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `feedback`
---
-
-INSERT INTO `feedback` (`FEEDBACK_ID`, `FEEDBACK_NAME`, `FEEDBACK_EMAIL`, `FEEDBACK_CONTACT`, `FEEDBACK_MESSAGE`, `FEEDBACK_TIME`) VALUES
-(1, 'dhruv', 'dhruvbdani@gmail.com', 'd', 'hi', '2021-03-14 21:24:08'),
-(2, 'd', 's@gmail.com', '', 'sdsdsdsd', '2021-03-14 21:24:08'),
-(3, 'dani dhruv', 'dhruvbdani@gmail.com', '', 'this is a latest feedback', '2021-03-14 21:28:29'),
-(4, 'Nilay', 'nilay2620@gmail.com', '', 'Good Awesome', '2021-04-18 16:40:10');
 
 -- --------------------------------------------------------
 
@@ -128,8 +118,10 @@ CREATE TABLE `game` (
 --
 
 INSERT INTO `game` (`GAME_ID`, `GAME_NAME`, `GAME_STATUS`, `GAME_DESCRIPTION`, `GAME_PROFILE_IMAGE`, `GAME_CATEGORY`) VALUES
-(1, 'TETRIS', 1, 'Tetris® is the addictive puzzle game that started it all, embracing our universal desire to create order out of chaos.', '\\backend assets\\images\\game\\tetris.png', 'DUMMY'),
-(2, 'SNAKE', 1, 'Snake is the common name for a video game concept where the player maneuvers a line which grows in length, with the line itself being a primary obstacle.', '\\backend assets\\images\\game\\snake.png', 'DUMMY');
+(1, 'Tetris', 1, 'Tetris is a easy game which can be played by anyone.', '', 'Fun loving'),
+(2, 'Snake', 1, 'Snake is a very common game which is being played by everyone once in a lifetime.', '', 'Entertainment'),
+(3, 'Memory game', 1, 'memory game is a game which test your memory.', '', 'mind game'),
+(4, 'Tic Tac Toe', 1, 'Tic Tac Toe is a game where 2 players can play together and compete with each other', '', '2D game');
 
 -- --------------------------------------------------------
 
@@ -155,10 +147,29 @@ CREATE TABLE `registration` (
   `REGISTRATION_ID` int(11) NOT NULL,
   `CLIENT_ID` int(11) NOT NULL,
   `TOURNAMENT_ID` int(11) NOT NULL,
-  `REGISTRATION_DATE` date NOT NULL,
-  `SANCTUM_TRANSACTION_ID` int(11) NOT NULL,
-  `REGISTRATION_AMOUNT` int(11) NOT NULL
+  `REGISTRATION_DATE` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `registration`
+--
+
+INSERT INTO `registration` (`REGISTRATION_ID`, `CLIENT_ID`, `TOURNAMENT_ID`, `REGISTRATION_DATE`) VALUES
+(1, 5, 1, '2021-04-12'),
+(2, 1, 2, '2021-05-06'),
+(3, 10, 3, '2021-06-09'),
+(4, 9, 1, '2021-04-05'),
+(5, 2, 3, '2021-06-09'),
+(6, 7, 2, '2021-06-07'),
+(7, 8, 1, '2021-04-04'),
+(8, 3, 2, '2021-05-10'),
+(9, 6, 3, '2021-06-11'),
+(10, 4, 2, '2021-05-08'),
+(11, 5, 3, '2021-06-01'),
+(12, 1, 1, '2021-04-12'),
+(13, 8, 3, '2021-06-09'),
+(14, 10, 1, '2021-04-12'),
+(15, 4, 3, '2021-06-08');
 
 -- --------------------------------------------------------
 
@@ -173,24 +184,6 @@ CREATE TABLE `result` (
   `REWARD_STATUS` tinyint(4) NOT NULL,
   `POSITION` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `result`
---
-
-INSERT INTO `result` (`RESULT_ID`, `TOURNAMENT_ID`, `CLIENT_ID`, `REWARD_STATUS`, `POSITION`) VALUES
-(1, 2, 1, 0, 1),
-(2, 2, 3, 0, 2),
-(3, 2, 2, 0, 3),
-(5, 3, 1, 0, 1),
-(10, 3, 3, 0, 3),
-(12, 3, 2, 0, 2),
-(14, 1, 3, 0, 1),
-(15, 1, 2, 0, 2),
-(16, 1, 1, 0, 3),
-(17, 6, 3, 0, 1),
-(18, 6, 1, 0, 2),
-(19, 6, 9, 0, 3);
 
 -- --------------------------------------------------------
 
@@ -211,17 +204,21 @@ CREATE TABLE `scoreboard` (
 --
 
 INSERT INTO `scoreboard` (`SCOREBOARD_ID`, `CLIENT_ID`, `GAME_ID`, `TOURNAMENT_ID`, `SCORE_TOTAL`) VALUES
-(1, 1, 1, 1, 50),
-(2, 1, 2, 1, 100),
-(3, 1, 1, 2, 50),
-(4, 3, 1, 1, 1000),
-(5, 1, 1, 1, 5000),
-(6, 1, 1, 1, 5000),
-(7, 3, 1, 1, 4000),
-(8, 2, 1, 1, 6000),
-(9, 2, 1, 1, 5000),
-(10, 3, 1, 1, 7000),
-(11, 3, 1, 6, 1000);
+(1, 5, 1, 2, 500),
+(2, 6, 3, 2, 900),
+(3, 1, 3, 3, 5000),
+(4, 9, 1, 2, 9000),
+(5, 1, 1, 2, 15000),
+(6, 5, 2, 3, 8000),
+(7, 9, 3, 1, 10000),
+(8, 7, 2, 1, 4000),
+(9, 10, 3, 2, 3000),
+(10, 10, 1, 3, 18000),
+(11, 4, 3, 1, 10000),
+(12, 2, 2, 2, 8000),
+(13, 7, 1, 2, 7500),
+(14, 7, 2, 3, 6000),
+(15, 4, 1, 1, 4000);
 
 -- --------------------------------------------------------
 
@@ -250,10 +247,9 @@ CREATE TABLE `tournament` (
 --
 
 INSERT INTO `tournament` (`TOURNAMENT_ID`, `TOURNAMENT_NAME`, `TOURNAMENT_START`, `TOURNAMENT_END`, `TOURNAMENT_STATUS`, `TOURNAMENT_SUBSCRIBERS`, `TOURNAMENT_TOKEN_PRICE`, `TOURNAMENT_DETAILS`, `TOURNAMENT_FIRST_PRIZE`, `TOURNAMENT_SECOND_PRIZE`, `TOURNAMENT_THIRD_PRIZE`, `TOURNAMENT_TREMS`, `TOURNAMENT_REGISTRATION_TILL`) VALUES
-(1, 'APRIL SEASON', '2021-03-02', '2021-03-31', 1, 0, 10, 'OUR VERY FIRST TOURNAMENT.ENJOY PEOPLE!!!', 100, 70, 40, 'THE ONLY CONDITION IS THAT THERE IS NO CONDITION.', '2021-02-28'),
-(2, 'CWL', '2021-02-01', '2021-02-28', 1, 0, 0, '', 0, 0, 0, '0K', '2021-03-31'),
-(3, 'JAN SEASON', '2021-01-01', '2021-01-31', 1, 0, 0, '', 0, 0, 0, 'LOL PPL THIS IS A DUMMY TEXT', '2020-12-31'),
-(6, 'Nilay Tha', '2021-04-16', '2021-04-17', 0, 0, 100, 'nothing', 102, 99, 100000, 'No terms', '2021-04-14');
+(1, 'April Season', '2021-04-22', '2021-04-29', 1, 5, 2000, 'This tournament is organized for fun.Even participants can win prizes.', 5000, 3000, 2000, 'you must register yourself before last registration date.', '2021-06-17'),
+(2, 'May Season', '2021-05-15', '2021-05-20', 1, 6, 1500, 'This tournament is not a normal tornament.It is a very big tournament.Even participants can win prizes.', 4000, 2000, 1500, 'you must register yourself before last registration date.', '2021-05-08'),
+(3, 'June Season', '2021-06-19', '2021-06-25', 1, 10, 2400, 'This tournament is organized by us for the sole purpose of enjoyment.Even participants can win prizes.', 3500, 1000, 800, 'you must register yourself before last registration date.', '2021-05-16');
 
 --
 -- Indexes for dumped tables
@@ -328,25 +324,25 @@ ALTER TABLE `tournament`
 -- AUTO_INCREMENT for table `administrator`
 --
 ALTER TABLE `administrator`
-  MODIFY `ADMIN_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ADMIN_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `client`
 --
 ALTER TABLE `client`
-  MODIFY `CLIENT_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `CLIENT_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `FEEDBACK_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `FEEDBACK_ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `game`
 --
 ALTER TABLE `game`
-  MODIFY `GAME_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `GAME_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `pwdreset`
@@ -358,25 +354,25 @@ ALTER TABLE `pwdreset`
 -- AUTO_INCREMENT for table `registration`
 --
 ALTER TABLE `registration`
-  MODIFY `REGISTRATION_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `REGISTRATION_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `result`
 --
 ALTER TABLE `result`
-  MODIFY `RESULT_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `RESULT_ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `scoreboard`
 --
 ALTER TABLE `scoreboard`
-  MODIFY `SCOREBOARD_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `SCOREBOARD_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `tournament`
 --
 ALTER TABLE `tournament`
-  MODIFY `TOURNAMENT_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `TOURNAMENT_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
